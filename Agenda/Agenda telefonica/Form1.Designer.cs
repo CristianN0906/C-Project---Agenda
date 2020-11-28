35bb3a64f4bb71765f46a49f9316f025eaead1f2
CristianN0906 <59765357+CristianN0906@users.noreply.github.com>
2020-11-28 12:19:00 +0200

    Add files via upload

---
+++ Agenda/Agenda telefonica/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Agenda_telefonica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serializareInformatiiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferinteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.windowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cautaPersoanaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serializareInformatiiToolStripMenuItem,
+            this.preferinteToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // serializareInformatiiToolStripMenuItem
+            // 
+            this.serializareInformatiiToolStripMenuItem.Name = "serializareInformatiiToolStripMenuItem";
+            this.serializareInformatiiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.serializareInformatiiToolStripMenuItem.Text = "Serializare informatii";
+            this.serializareInformatiiToolStripMenuItem.Click += new System.EventHandler(this.serializareInformatiiToolStripMenuItem_Click);
+            // 
+            // preferinteToolStripMenuItem
+            // 
+            this.preferinteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buttonsToolStripMenuItem,
+            this.windowsToolStripMenuItem,
+            this.dataGridToolStripMenuItem});
+            this.preferinteToolStripMenuItem.Name = "preferinteToolStripMenuItem";
+            this.preferinteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.preferinteToolStripMenuItem.Text = "Customization";
+            // 
+            // buttonsToolStripMenuItem
+            // 
+            this.buttonsToolStripMenuItem.Name = "buttonsToolStripMenuItem";
+            this.buttonsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.buttonsToolStripMenuItem.Text = "Buttons";
+            this.buttonsToolStripMenuItem.Click += new System.EventHandler(this.buttonsToolStripMenuItem_Click);
+            // 
+            // windowsToolStripMenuItem
+            // 
+            this.windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            this.windowsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.windowsToolStripMenuItem.Text = "Windows";
+            this.windowsToolStripMenuItem.Click += new System.EventHandler(this.windowsToolStripMenuItem_Click);
+            // 
+            // dataGridToolStripMenuItem
+            // 
+            this.dataGridToolStripMenuItem.Name = "dataGridToolStripMenuItem";
+            this.dataGridToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.dataGridToolStripMenuItem.Text = "Data Grid";
+            this.dataGridToolStripMenuItem.Click += new System.EventHandler(this.dataGridToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cautaPersoanaToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // cautaPersoanaToolStripMenuItem
+            // 
+            this.cautaPersoanaToolStripMenuItem.Name = "cautaPersoanaToolStripMenuItem";
+            this.cautaPersoanaToolStripMenuItem.Size = new System.Drawing.Size(147, 22);
+            this.cautaPersoanaToolStripMenuItem.Text = "Person search";
+            this.cautaPersoanaToolStripMenuItem.Click += new System.EventHandler(this.cautaPersoanaToolStripMenuItem_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 399);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(87, 39);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(105, 399);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(87, 39);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(665, 182);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(611, 424);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(30, 13);
+            this.lblTime.TabIndex = 4;
+            this.lblTime.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Agenda Telefonica";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serializareInformatiiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preferinteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cautaPersoanaToolStripMenuItem;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem buttonsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem windowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataGridToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ Agenda/Agenda telefonica/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+using System.Configuration;
+
+namespace Agenda_telefonica
+{
+    public partial class Form1 : Form
+    {
+        // TODO in future - connection throuh App.config
+        string connConfig = ConfigurationManager.ConnectionStrings["dbConn"].ConnectionString;
+
+        // Declaration of variables
+        public string connSettings = @"Server=DESKTOP-4MH1ERN\SQLEXPRESS;Database=Agenda;Trusted_Connection=True"; 
+        private SqlDataAdapter daDB;
+        private DataSet dsDB;
+        private SqlConnection conn;
+        UserPreferences uPref = new UserPreferences();
+        BinaryFormatter binFormat = new BinaryFormatter();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.ReadOnly = false;
+
+            // Verify if "user.dat" exists and deserialize the preferences stored there
+            if (File.Exists(@"user.dat"))
+            {
+                binFormat = new BinaryFormatter();
+
+                object deS = binFormat.Deserialize(new FileStream("user.dat", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                uPref = (UserPreferences)deS;
+
+                // Buttons color
+                if (!uPref.preferences[0].IsEmpty)
+                    foreach (Button btn in Controls.OfType<Button>())
+                        btn.BackColor = uPref.preferences[0];
+
+                //backcolor of the form
+                if (!uPref.preferences[1].IsEmpty)
+                    this.BackColor = uPref.preferences[1];
+
+                // datagridview background color
+                if (!uPref.preferences[2].IsEmpty)
+                    dataGridView1.BackgroundColor = uPref.preferences[2];
+
+            }
+
+            timer1.Start();
+        }
+
+        // BUTTONS
+
+        // Load Button
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string command = "SELECT * FROM Persoane";
+
+                if (dsFillDB(command))
+                {
+                    SqlCommandBuilder cmdBldr = new SqlCommandBuilder(daDB);
+
+                    dataGridView1.DataSource = dsDB;
+                    dataGridView1.DataMember = "Person";
+
+                    //Verify if the database is empty
+                    if (dataGridView1.RowCount == 1)
+                    {
+                        DialogResult dialog = MessageBox.Show("DataBase checked!","No subscribers found in DataBase!", MessageBoxButtons.OK);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error on Load: " + ex);
+            }
+        }
+
+        //Save Button
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                daDB.Update(dsDB, "Person");
+                MessageBox.Show("DataBase have been updated!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error:" + ex);
+            }
+        }
+
+        // MENU
+        
+        // Customization
+        //buttons color
+        private void buttonsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (Button btn in Controls.OfType<Button>())
+                       btn.BackColor = colorDialog1.Color;
+                   
+                uPref.preferences[0] = colorDialog1.Color;
+            }
+        }
+
+        //windows color
+        private void windowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+                uPref.preferences[1] = colorDialog1.Color;
+            }
+        }
+
+        //DGV background-color
+        private void dataGridToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dataGridView1.BackgroundColor = colorDialog1.Color;
+                uPref.preferences[2] = colorDialog1.Color;
+            }
+            
+        }
+        
+        // EXIT
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Do you want to close the program?", "Exit", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+                
+
+        //Serialization of information
+        private void serializareInformatiiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((dsDB != null) || (dataGridView1.RowCount > 1))
+                {
+
+                    //string filepathname = "Subscribers_" + DateTime.Now.Year + "." + DateTime.Now.Month + "." + DateTime.Now.Day + "." + DateTime.Now.Hour + "." + DateTime.Now.Minute + "." + DateTime.Now.Second + ".xml";
+                    string filepath = @"..\..\..\" + "Subscribers_" + DateTime.Now.Year + "." + DateTime.Now.Month + "." + DateTime.Now.Day + "." + DateTime.Now.Hour + "." + DateTime.Now.Minute + "." + DateTime.Now.Second + ".xml";
+
+                    
+
+                    //saveFileDialog2.Filter = "File XML| *.xml";
+                    //saveFileDialog2.Title = "Save data";
+                    //saveFileDialog2.FileName = filepathname;
+
+
+                    //if (saveFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    //{
+                        XmlTextWriter xmlSave = new XmlTextWriter(filepath, Encoding.UTF8);
+                        xmlSave.Formatting = Formatting.Indented;
+                        dsDB.DataSetName = "Data";
+                        dsDB.WriteXml(xmlSave);
+                        xmlSave.Close();
+                        MessageBox.Show("The file was saved!");
+                    //}
+                    //else
+                    //{
+                    //    MessageBox.Show("Error: The file was not saved!");
+                    //}
+
+
+                }
+                else
+                {
+                    MessageBox.Show("DataGridView is empty!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error!" + ex);
+            }
+
+        }
+
+        // Person Search
+        private void cautaPersoanaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Form formSearch = new Form();
+            formSearch.Visible = true;
+            formSearch.Location = new Point(100, 100);
+            formSearch.Width = 800;
+            formSearch.Height = 400;
+            formSearch.SuspendLayout();
+
+            DataGridView DGVSearch = new DataGridView();
+            DGVSearch.Location = new Point(10, 100);
+            DGVSearch.Size = new Size(600, 200);
+            DGVSearch.Visible = true;
+            DGVSearch.ReadOnly = true;
+            formSearch.Controls.Add(DGVSearch);
+
+            TextBox txtSearch = new TextBox();
+            txtSearch.Location = new Point(10, 10);
+            txtSearch.Visible = true;
+            txtSearch.Width = 200;
+            formSearch.Controls.Add(txtSearch);
+
+            Button btnSearch = new Button();
+            btnSearch.Location = new Point(10, 50);
+            btnSearch.Visible = true;
+            btnSearch.Text = "Search";
+            formSearch.Controls.Add(btnSearch);
+
+            // Create an event when the button is pressed
+            //btnSearch.Click += new EventHandler(btnSearch_Click, txtSearch.Text);
+            btnSearch.Click += new EventHandler((evsender, ex) => btnSearch_Click(evsender, ex, txtSearch.Text, DGVSearch));
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e, string text, DataGridView DGVSearch)
+        {
+            try
+            {
+                string command = "SELECT * FROM Persoane WHERE Nume = '" + text + "' OR Prenume = '" + text + "' OR Telefon = '" + text + "' OR Oras = '" + text + "'";
+
+                if (dsFillDB(command))
+                {
+                    SqlCommandBuilder cmdBldr = new SqlCommandBuilder(daDB);
+
+                    DGVSearch.DataSource = dsDB;
+                    DGVSearch.DataMember = "Person";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error on Load: " + ex);
+            }
+        }
+
+        // method which bring the information from database into DataSource, into DataAdapter
+        private bool dsFillDB(string command)
+        {
+            try
+            {
+                dataGridView1.DataSource = null;
+                conn = new SqlConnection(connSettings);
+                dsDB = new DataSet();
+                daDB = new SqlDataAdapter(command, connSettings);
+
+                daDB.Fill(dsDB, "Person");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error at connection to DB: " + ex);
+                return false;
+            }
+        }
+
+        // Save preferences when the program is closed
+        public void SavePrefIntoFile()
+        {
+            //The object is saved into a file named user.dat
+
+            using (Stream fStream = new FileStream("user.dat",
+                                                    FileMode.Create,
+                                                    FileAccess.Write,
+                                                    FileShare.ReadWrite))
+            {
+                binFormat.Serialize(fStream, uPref);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SavePrefIntoFile();
+        }
+
+        //time ticker
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dateTime = DateTime.Now;
+            this.lblTime.Text = dateTime.ToString();
+        }
+    }
+
+    [Serializable]
+    public class UserPreferences
+    {
+
+        public Color[] preferences = new Color[3];
+        //preferences[0] saves button BackGround color
+        //preferences[1] saves datagridview BackGround color
+        //preferences[2] saves Form backGround color
+        
+        //TODO
+        public string font;
+                              
+        public UserPreferences()
+        {
+            
+        }
+
+    }
+
+    // TODO in future - create a class which store methods to manipulate the database commands
+    public class DataBaseWork
+    { 
+    
+    }
+}
